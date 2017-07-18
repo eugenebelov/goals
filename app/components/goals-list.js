1b34eba26f27498ff11d305f8f909e18b9e687a1
Eugene Belov <eugene.belov@smashdocs.net>
2017-07-19 00:28:16 +0300

    add html markup for goals list; add goals list to personelist

--- app/components/goals-list.js
+++ app/components/goals-list.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['goals-list-component'],
+  goals: [],
+  actions: {
+    onAddGoal () {
+      
+    }
+  }
 });

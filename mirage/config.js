6d74b1490a030bf9733d0780f3625093d3e05161
Eugene Belov <eugene.belov@smashdocs.net>
2017-07-19 00:20:13 +0300

    add post to mirage

--- mirage/config.js
+++ mirage/config.js
@@ -26,6 +26,7 @@ export default function() {
   });
 
   this.post('/goals')
+  this.post('/persones')
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced

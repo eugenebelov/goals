577b526cac897595d21a01601d0ba6299b9af178
Eugene Belov <eugene.belov@smashdocs.net>
2017-07-19 00:21:16 +0300

    add goals component

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,10 +3,12 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     save(data) {
-        let model = this.get('model').persones,
-            person = this.get('store').createRecord('persone', data);
-
-            console.log(model);
-      }
+      let person = this.get('store').createRecord('persone', data);
+      person.save();
+    },
+    addGoal(data) {
+      let goal = this.get('store').createRecord('goal', data);
+      goal.save()
+    }
   }
 });

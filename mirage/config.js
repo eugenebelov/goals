50cb6a8f5e6c5cfe07328e3f3639e6658a46225f
Eugene Belov <eugene.belov@smashdocs.net>
2017-07-19 00:10:15 +0300

    add controller for app

--- mirage/config.js
+++ mirage/config.js
@@ -8,6 +8,25 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
+  this.get('/persones');
+  this.get('/goals', {
+    'data': [{
+      "id":"28",
+      "attributes":{
+        "title":"goal1",
+        "score": 1},
+        "type":"goals"
+      }, {
+        "id":"29",
+        "attributes":{
+          "title":"goal2",
+          "score": 2},
+          "type":"goals"
+        }]
+  });
+
+  this.post('/goals')
+
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing

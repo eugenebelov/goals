e9f38145c2264942e0be408477de91a7e28ea435
Eugene Belov <eugene.belov@smashdocs.net>
2017-07-19 00:17:19 +0300

    add persone to list

--- app/components/persone-list.js
+++ app/components/persone-list.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['persone-list-component'],
+  persone: [],
+  goal:[],
+  actions: {
+    onAddGoalToPersone() {
+
+    },
+
+    onAddPersone () {
+      this.sendAction('save', {
+        'id': this.get('persone').content.length + 1,
+        'firstName': this.get('personeFirstName'),
+        'lastName': this.get('personeLastName')
+      });
+    }
+  }
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  
+  actions: {
+    save(data) {
+        let model = this.get('model').persones,
+            person = this.get('store').createRecord('persone', data);
+
+            console.log(model);
+      }
+  }
 });
